d5b3f3d42cd60abdaca63f37027a9da8e1081f17
Klaus <direjor2010@yandex.ru>
2024-07-16 22:27:46 +0300

    Update class.h
    more warnings = faster runtime

--- SRC/class.h
+++ SRC/class.h
@@ -16,21 +16,15 @@
 #define CLASS_COMPILE(_name_) \
     void *M_JOIN(_impl_,_name_)(const char,CLASS _name_* _CLASS_ARGS_STD(_name_) );\
     void M_JOIN(_dtor_,_name_)(void* self){\
-        void(* const f)(const char,void*)=(void*)M_JOIN(_impl_,_name_);\
-        f(0,self);\
-        M_WHEN(_CLASS_EXT(_name_))({\
-            extern void M_JOIN(_dtor_,_CLASS_EXT(_name_))(void*);\
-            M_JOIN(_dtor_,_CLASS_EXT(_name_))(self);\
-        })\
+        _CLASS_LOOP(_CLASS_DESTRUCT(_name_,self))\
     }\
     void *M_JOIN(_ctor_,_name_)(void *self _CLASS_ARGS_VAR(_name_) ){\
         if(self){\
-            void*(* const f)(const char,void* _CLASS_ARGS_VAR(_name_))=(void*)M_JOIN(_impl_,_name_);\
             M_IF(_CLASS_EXT(_name_))(\
                 _CLASS_CLEAR(((char*)self)+sizeof(CLASS _CLASS_EXT(_name_)),sizeof(CLASS _name_)-sizeof(CLASS _CLASS_EXT(_name_))) ,\
                 _CLASS_CLEAR(self,sizeof(CLASS _name_))\
             )\
-            if(f(1,self _CLASS_ARGS_CALL(_name_,)))\
+            if( ((void*(*)(const char,void* _CLASS_ARGS_VAR(_name_)))M_JOIN(_impl_,_name_)) (1,self _CLASS_ARGS_CALL(_name_,)) )\
                 *(void**)self=M_JOIN(_dtor_,_name_);\
             else{ M_JOIN(_dtor_,_name_)(self); self=(void*)0; }\
         } return self;\
@@ -151,6 +145,12 @@
         extern void *M_JOIN(_ctor_,_CLASS_EXT(_name_))(void* _CLASS_ARGS_VAR(_CLASS_EXT(_name_)));\
         void *(* const super)(void* _CLASS_ARGS_STD(_CLASS_EXT(_name_)))=(void*)M_JOIN(_ctor_,_CLASS_EXT(_name_));\
     )
+
+#define __CLASS_DESTRUCT() _CLASS_DESTRUCT
+#define _CLASS_DESTRUCT(_name_,_self_) \
+    {   extern void *M_JOIN(_impl_,_name_)(const char,CLASS _name_* _CLASS_ARGS_STD(_name_) );\
+        ((void(*)(const char, void *))M_JOIN(_impl_,_name_))(0,_self_);\
+    } M_WHEN(_CLASS_EXT(_name_))( M_OBSTRUCT(__CLASS_DESTRUCT)()(_CLASS_EXT(_name_),_self_) ) 
 
 #define __CLASS_END() _CLASS_END
 #define _CLASS_END(_name_,...)\
