06c364bb16378caa6e319d310cf2391a277a8b9e
Klaus <direjor2010@yandex.ru>
2024-08-06 00:54:54 +0300

    Update _KLS_string.c
    rewrite stringSolve part

--- SRC/modules/_KLS_string.c
+++ SRC/modules/_KLS_string.c
@@ -77,13 +77,19 @@ const char *_KLS_stringSep(const char **string,unsigned int *len,const char * co
     return x;
 }
 
+typedef union{
+    const char *c;
+    char *v;
+}_KLS_t_SOLVE_PTR;
+
 typedef struct{
-    char *left, *op, *right;
+    _KLS_t_SOLVE_PTR op, left, right, beg, end;
 }_KLS_t_SOLVE_MATCH;
 
 typedef struct{
-    char *str, *breckets, *unBreckets;
-    char *tmp, *tmp2, *tmp3;
+    char *str, *value;
+    _KLS_t_SOLVE_PTR brecket;
+    _KLS_t_SOLVE_MATCH op;
     char precision[8];
 }_KLS_t_SOLVE;
 
@@ -124,176 +130,167 @@ KLS_size _KLS_solveFac(KLS_size x){
     return res;
 }
 
-const char *_KLS_solveCalcOp(const char *frmt,const char *l,const char *op, const char *r, char *tmp){
+int _KLS_solveOpValue(const char *frmt,const char *l,const char *op, const char *r, char *tmp){
     KLS_byte chk=(!!l) | ((!!r)<<1);
-    sprintf(tmp,"err");
     switch(*op){
-        case '+': sprintf(tmp,frmt,strtod(l,0) + strtod(r,0)); return tmp;
-        case '-': sprintf(tmp,frmt,strtod(l,0) - strtod(r,0)); return tmp;
-        case '*': sprintf(tmp,frmt,strtod(l,0) * strtod(r,0)); return tmp;
-        case '/': sprintf(tmp,frmt,strtod(l,0) / strtod(r,0)); return tmp;
-        case '@': sprintf(tmp,frmt,pow(strtod(l,0),strtod(r,0))); return tmp;
-        case '%': sprintf(tmp,frmt,KLS_mod(strtod(l,0),strtod(r,0))); return tmp;
+        case '+': return sprintf(tmp,frmt,strtod(l,0) + strtod(r,0));
+        case '-': return sprintf(tmp,frmt,strtod(l,0) - strtod(r,0));
+        case '*': return sprintf(tmp,frmt,strtod(l,0) * strtod(r,0));
+        case '/': return sprintf(tmp,frmt,strtod(l,0) / strtod(r,0));
+        case '@': return sprintf(tmp,frmt,pow(strtod(l,0),strtod(r,0)));
+        case '%': return sprintf(tmp,frmt,KLS_mod(strtod(l,0),strtod(r,0)));
         case '~':{
             double vr=strtod(r,0);
-            if(vr<=INT_MAX) sprintf(tmp,"%d",~(int)vr);
-            return tmp;
+            if(vr<=INT_MAX) return sprintf(tmp,"%d",~(int)vr);
+            return -2;
         }
         case '^':{
             double vl=strtod(l,0),vr=strtod(r,0);
-            if(vl<=INT_MAX && vr<=INT_MAX) sprintf(tmp,"%d",(int)vl ^ (int)vr);
-            return tmp;
+            if(vl<=INT_MAX && vr<=INT_MAX) return sprintf(tmp,"%d",(int)vl ^ (int)vr);
+            return -2;
         }
         case '!':
             switch(chk){
                 case 1:{
                     double v=strtod(l,0);
-                    if(v<0) sprintf(tmp,"-%.0f" ,1.e-6+_KLS_solveFac(-v));
-                    else sprintf(tmp,"%.0f",1.e-6+_KLS_solveFac(v));
-                    return tmp;
+                    if(v<0) return sprintf(tmp,"-%.0f" ,1.e-6+_KLS_solveFac(-v));
+                    else return sprintf(tmp,"%.0f",1.e-6+_KLS_solveFac(v));
                 }
-                case 2: tmp[0]='0'+!strtod(r,0); tmp[1]=0; return tmp; // logical negative
-                case 3: break;
+                case 2: tmp[0]='0'+!strtod(r,0); tmp[1]=0; return 1; // logical negative
+                default: return -2;
             }
     }
-    if(!strncmp(op,"log10",5)) {sprintf(tmp,frmt,log10(strtod(r,0)*M_PI/180.)); return tmp;}
-    if(!strncmp(op,"log2",4)) {sprintf(tmp,frmt,log2(strtod(r,0)*M_PI/180.)); return tmp;}
-    if(!strncmp(op,"asin",4)) {sprintf(tmp,frmt,asin(strtod(r,0)*M_PI/180.)); return tmp;}
-    if(!strncmp(op,"acos",4)) {sprintf(tmp,frmt,acos(strtod(r,0)*M_PI/180.)); return tmp;}
-    if(!strncmp(op,"atan",4)) {sprintf(tmp,frmt,atan(strtod(r,0)*M_PI/180.)); return tmp;}
-    if(!strncmp(op,"log",3)) {sprintf(tmp,frmt,log(strtod(r,0)*M_PI/180.)); return tmp;}
-    if(!strncmp(op,"sin",3)) {sprintf(tmp,frmt,sin(strtod(r,0)*M_PI/180.)); return tmp;}
-    if(!strncmp(op,"cos",3)) {sprintf(tmp,frmt,cos(strtod(r,0)*M_PI/180.)); return tmp;}
-    if(!strncmp(op,"tan",3)) {sprintf(tmp,frmt,tan(strtod(r,0)*M_PI/180.)); return tmp;}
-    if(!strncmp(op,"||",2)) {tmp[0]='0'+(strtod(l,0) || strtod(r,0)); tmp[1]=0; return tmp;}
-    if(!strncmp(op,"&&",2)) {tmp[0]='0'+(strtod(l,0) && strtod(r,0)); tmp[1]=0; return tmp;}
-    if(!strncmp(op,">=",2)) {tmp[0]='0'+(strtod(l,0) >= strtod(r,0)); tmp[1]=0; return tmp;}
-    if(!strncmp(op,"<=",2)) {tmp[0]='0'+(strtod(l,0) <= strtod(r,0)); tmp[1]=0; return tmp;}
-    if(!strncmp(op,"==",2)) {tmp[0]='0'+(strtod(l,0) == strtod(r,0)); tmp[1]=0; return tmp;}
-    if(!strncmp(op,"!=",2)) {tmp[0]='0'+(strtod(l,0) != strtod(r,0)); tmp[1]=0; return tmp;}
+    if(!strncmp(op,"log10",5)) return sprintf(tmp,frmt,log10(strtod(r,0)*M_PI/180.));
+    if(!strncmp(op,"log2",4)) return sprintf(tmp,frmt,log2(strtod(r,0)*M_PI/180.));
+    if(!strncmp(op,"asin",4)) return sprintf(tmp,frmt,asin(strtod(r,0)*M_PI/180.));
+    if(!strncmp(op,"acos",4)) return sprintf(tmp,frmt,acos(strtod(r,0)*M_PI/180.));
+    if(!strncmp(op,"atan",4)) return sprintf(tmp,frmt,atan(strtod(r,0)*M_PI/180.));
+    if(!strncmp(op,"log",3)) return sprintf(tmp,frmt,log(strtod(r,0)*M_PI/180.));
+    if(!strncmp(op,"sin",3)) return sprintf(tmp,frmt,sin(strtod(r,0)*M_PI/180.));
+    if(!strncmp(op,"cos",3)) return sprintf(tmp,frmt,cos(strtod(r,0)*M_PI/180.));
+    if(!strncmp(op,"tan",3)) return sprintf(tmp,frmt,tan(strtod(r,0)*M_PI/180.));
+    if(!strncmp(op,"||",2)) {tmp[0]='0'+(strtod(l,0) || strtod(r,0)); tmp[1]=0; return 1;}
+    if(!strncmp(op,"&&",2)) {tmp[0]='0'+(strtod(l,0) && strtod(r,0)); tmp[1]=0; return 1;}
+    if(!strncmp(op,">=",2)) {tmp[0]='0'+(strtod(l,0) >= strtod(r,0)); tmp[1]=0; return 1;}
+    if(!strncmp(op,"<=",2)) {tmp[0]='0'+(strtod(l,0) <= strtod(r,0)); tmp[1]=0; return 1;}
+    if(!strncmp(op,"==",2)) {tmp[0]='0'+(strtod(l,0) == strtod(r,0)); tmp[1]=0; return 1;}
+    if(!strncmp(op,"!=",2)) {tmp[0]='0'+(strtod(l,0) != strtod(r,0)); tmp[1]=0; return 1;}
     if(!strncmp(op,"<<",2)) {
         double vl=strtod(l,0), vr=strtod(r,0);
-        if(vl<=INT_MAX && vr<=INT_MAX) sprintf(tmp,"%d",((int)vl) << ((int)vr));
-        return tmp;
+        if(vl<=INT_MAX && vr<=INT_MAX) return sprintf(tmp,"%d",((int)vl) << ((int)vr));
+        return -2;
     }
     if(!strncmp(op,">>",2)) {
         double vl=strtod(l,0), vr=strtod(r,0);
-        if(vl<=INT_MAX && vr<=INT_MAX) sprintf(tmp,"%d",((int)vl) << ((int)vr));
-        return tmp;
+        if(vl<=INT_MAX && vr<=INT_MAX) return sprintf(tmp,"%d",((int)vl) << ((int)vr));
+        return -2;
     }
 
-    if(*op=='>') {tmp[0]='0'+(strtod(l,0) > strtod(r,0)); tmp[1]=0; return tmp;}
-    if(*op=='<') {tmp[0]='0'+(strtod(l,0) < strtod(r,0)); tmp[1]=0; return tmp;}
+    if(*op=='>') {tmp[0]='0'+(strtod(l,0) > strtod(r,0)); tmp[1]=0; return 1;}
+    if(*op=='<') {tmp[0]='0'+(strtod(l,0) < strtod(r,0)); tmp[1]=0; return 1;}
     if(*op=='&') {
         double vl=strtod(l,0),vr=strtod(r,0);
-        if(vl<=INT_MAX && vr<=INT_MAX) sprintf(tmp,"%d",(int)vl & (int)vr);
-        return tmp;
+        if(vl<=INT_MAX && vr<=INT_MAX) return sprintf(tmp,"%d",(int)vl & (int)vr);
+        return -2;
     }
     if(*op=='|') {
         double vl=strtod(l,0),vr=strtod(r,0);
-        if(vl<=INT_MAX && vr<=INT_MAX) sprintf(tmp,"%d",(int)vl | (int)vr);
-        return tmp;
+        if(vl<=INT_MAX && vr<=INT_MAX) return sprintf(tmp,"%d",(int)vl | (int)vr);
+        return -2;
     }
-    return tmp;
+    return -1;
 }
 
-KLS_byte _KLS_solveReplace(char *str,const char *from,const char *to,char *tmp){
-    if(str && from && to){
-        unsigned int len=strlen(from);
-        const char *begin=str,*end;
-        *tmp=0;
-        /*printf("replace in %s: \'%s\' to \'%s\'\n",str,from,to);*/
-        while((end=strstr(begin,from))){
-            strncat(tmp,begin,end-begin);
-            strcat(tmp,to);
-            begin=end+len;
-        }
-        if(begin==str) return 0;
-        strcat(tmp,begin);
-        *str=0; strcat(str,tmp);
+char _KLS_solveOpCalc(_KLS_t_SOLVE * const s){
+    const int lenTo=_KLS_solveOpValue(s->precision,s->op.left.c,s->op.op.c,s->op.right.c,s->value);
+    if(lenTo>0){
+        const int lenFrom=s->op.end.c-s->op.beg.c, lenDiff=lenTo-lenFrom, lenRest=strlen(s->op.end.c)+1;
+        memmove(s->op.end.v+lenDiff,s->op.end.c,lenRest);
+        memcpy(s->op.beg.v,s->value,lenTo);
         return 1;
-    }
-    return 0;
+    } return 0;
 }
 
-KLS_byte _KLS_solveGetBreckets(_KLS_t_SOLVE *s){
-    if(s->str){
-        char l;
-        const char *begin=s->str,*str=begin;
-        while( (l=*str) ){
-            if(l=='('){
-                begin=str++;
-                continue;
-            }
-            if(l==')'){
-                unsigned int len=(int)(str-begin)+1;
-                *s->breckets=0; strncat(s->breckets,begin,len);
-                *s->unBreckets=0; strncat(s->unBreckets,begin+1,len-2);
-                return 1;
-            }
-            ++str;
+char _KLS_solveBrecket(_KLS_t_SOLVE * const s){
+    s->brecket.c=NULL;
+    const char *str=s->str;
+    while(1){
+        switch(*str){
+            case 0: return 0;
+            case '(': s->brecket.c=str; break;
+            case ')': return !!s->brecket.c;
         }
+        ++str;
     }
     return 0;
 }
 
-const char *_KLS_solveFindLeft(const char *begin){
-    char c;
-    KLS_byte cnt=0;
-    while( (c=*(--begin)) && ((c>='0' && c<='9') || c=='.') ) ++cnt;
-    if(!cnt) return NULL;
-    if(c!='-') ++begin;
-    return begin;
+const char *_KLS_solveOpLeft(const char *s){
+    char c, flag=0;
+    while( (c=*(--s)) && ((c>='0' && c<='9') || c=='.') ) flag|=1;
+    if(!flag) return NULL;
+    if(c!='-') ++s;
+    return s;
 }
 
-const char *_KLS_solveFindEnd(const char *end){
-    char c=*(end++);
-    KLS_byte cnt=0;
-    if(c>='0' && c<='9') ++cnt;
+const char *_KLS_solveOpEnd(const char *s){
+    char c=*(s++), flag=0;
+    if(c>='0' && c<='9') flag|=1;
     else if(c!='-') return NULL;
-    while( (c=*(end++)) && ((c>='0' && c<='9') || c=='.') ) ++cnt;
-    if(cnt) return --end;
+    while( (c=*(s++)) && ((c>='0' && c<='9') || c=='.') ) flag|=1;
+    if(flag) return s-1;
     return NULL;
 }
 
-KLS_byte _KLS_solveFindOp(const char *str,const _KLS_t_SOLVE_OP *op,_KLS_t_SOLVE_MATCH *m, char *replace){
-    const char *begin, *end;
-    const _KLS_t_SOLVE_OP *o;
-    *replace=0;
-    memset(m,0,sizeof(*m));
-    for(;*str;++str)
+KLS_byte _KLS_solveOpFind(_KLS_t_SOLVE * const s,const _KLS_t_SOLVE_OP * const op){
+    const _KLS_t_SOLVE_OP *o=op;
+    const char *str=s->brecket.c+1;
+    for(;*str!=')';++str)
         for(o=op;o->op;++o)
             if(!strncmp(str,o->op,o->len)){
-                begin=str;
-                end=begin+o->len;
                 if(o->left){
-                    if( !(begin=_KLS_solveFindLeft(begin)) )
+                    if( !(s->op.left.c=_KLS_solveOpLeft(str)) )
                         break;
-                    m->left=begin;
+                    s->op.beg.c=s->op.left.c;
+                }else{
+                    s->op.left.c=NULL;
+                    s->op.beg.c=str;
                 }
                 if(o->right){
-                    m->right=end;
-                    if( !(end=_KLS_solveFindEnd(end)) ){
-                        m->right=NULL;
+                    s->op.right.c=str+o->len;
+                    if( !(s->op.end.c=_KLS_solveOpEnd(s->op.right.c)) )
                         break;
-                    }
+                }else{
+                    s->op.right.c=NULL;
+                    s->op.end.c=str+o->len;
                 }
-                m->op=o->op;
-                strncat(replace,begin,end-begin);
+                s->op.op.c=o->op;
                 return 1;
             }
     return 0;
 }
 
-KLS_byte _KLS_solveTry(_KLS_t_SOLVE *s){
+void _KLS_solveUnbrecket(_KLS_t_SOLVE * const solve){
+    char *s=solve->brecket.v;
+    const char *fwd=s+1;
+    while(*fwd!=')'){
+        *s=*fwd; ++s; ++fwd;
+    }
+    ++fwd;
+    while(*fwd){
+        *s=*fwd; ++s; ++fwd;
+    }
+    *s=0;
+}
+
+KLS_byte _KLS_solveTry(_KLS_t_SOLVE * const s){
     unsigned int i;
-    _KLS_t_SOLVE_MATCH m;
-    while(_KLS_solveGetBreckets(s)){
+    while(_KLS_solveBrecket(s)){
         for(i=0;i<KLS_ARRAY_LEN(_KLS_solveOps);++i)
-            while(_KLS_solveFindOp(s->unBreckets,_KLS_solveOps[i],&m,s->tmp2))
-                if( !_KLS_solveReplace(s->unBreckets,s->tmp2,_KLS_solveCalcOp(s->precision,m.left,m.op,m.right,s->tmp3),s->tmp) )
+            while(_KLS_solveOpFind(s,_KLS_solveOps[i]))
+                if(!_KLS_solveOpCalc(s))
                     return 0;
-        _KLS_solveReplace(s->str,s->breckets,s->unBreckets,s->tmp);
+        _KLS_solveUnbrecket(s);
     }
     return 1;
 }
@@ -319,14 +316,13 @@ void _KLS_solveRemoveSymbols(char *s,const char *symbols){
     while(*s){
         for(c=symbols;*c;++c)
             if(*s==*c){
-                fwd=s+1;
-                goto _mark;
+                fwd=s+1; goto _mark;
             }
         ++s;
     }
     return;
 _mark:
-    while(*s){
+    while(*fwd){
         for(c=symbols;*c;++c)
             if(*fwd==*c){
                 ++fwd; goto _mark;
@@ -339,19 +335,13 @@ _mark:
 }
 
 KLS_byte _KLS_solveMake(const char *str,_KLS_t_SOLVE *s,int precision){
-    memset(s,0,sizeof(*s));
     sprintf(s->precision,"%%.%df",precision<0?6:precision);
     if(str){
-        unsigned int len1=(strlen(str)+_KLS_solveAddLen(str,s->precision)+3+3), len2=128;
-        if( (s->str=KLS_malloc((len1<<2)+(len2<<1))) ){
-            *s->str=0;
-            s->breckets=s->str+len1;         s->breckets[-1]=s->breckets[0]=0;
-            s->unBreckets=s->breckets+len1;  s->unBreckets[-1]=s->unBreckets[0]=0;
-            s->tmp=s->unBreckets+len1;       s->tmp[-1]=s->tmp[0]=0;
-            s->tmp2=s->tmp+len1;             s->tmp2[-1]=s->tmp2[0]=0;
-            s->tmp3=s->tmp2+len2;            s->tmp3[-1]=s->tmp3[0]=0;
+        const unsigned int len=(strlen(str)+_KLS_solveAddLen(str,s->precision)+100);
+        if( (s->str=KLS_malloc(len+1)) ){
+            s->value=s->str+len-65;
             sprintf(s->str,"(%s)",str);
-            _KLS_solveRemoveSymbols(s->str,"\t\n ");
+            _KLS_solveRemoveSymbols(s->str,"\t\n\r ");
             return 1;
         }
     }
@@ -359,7 +349,7 @@ KLS_byte _KLS_solveMake(const char *str,_KLS_t_SOLVE *s,int precision){
 }
 
 KLS_byte _KLS_solveFindVal(const char *str){
-    int n=-2; return sscanf(str,"%*f%n",&n)==0 && n==strlen(str);
+    int n=-2; return sscanf(str,"%*f%n",&n)==0 && n==(int)strlen(str);
 }
 
 double KLS_stringSolve(const char *str,int floatPrecision){
