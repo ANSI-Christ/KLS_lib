cb2355e7356fa90ec8b64fb00b7e408fc02e5e59
Klaus <ansichrist@yandex.ru>
2024-11-17 00:53:26 +0300

    Update outmain.h

--- SRC/outmain.h
+++ SRC/outmain.h
@@ -1,57 +1,72 @@
 #include "macro.h"
 
 #ifdef OUTMAIN_SYNTAX
-    #include OUTMAIN_SYNTAX
-    #define _OUTMAIN_SYNTAX 0 /* by user */
+    #define _OUTMAIN_SYNTAX 0
 #endif
 
 #ifndef _OUTMAIN_SYNTAX
     /* detecting compiller */
-    #define _OUTMAIN_SYNTAX 4
+    #define _OUTMAIN_SYNTAX 1
 #endif
 
+/*****************   USER   **************/
+#if _OUTMAIN_SYNTAX == 0
+    #include OUTMAIN_SYNTAX
+#endif
 
-
-#if _OUTMAIN_SYNTAX == 1 /* GCC */
+/*****************   GCC   **************/
+#if _OUTMAIN_SYNTAX == 1
     #ifdef OUTMAIN_BEFORE
-        __attribute__((__constructor__)) static void M_JOIN(_OM_,M_PEAK(OUTMAIN_BEFORE))(void){ M_REMAINED(OUTMAIN_BEFORE); }
+        __attribute__((__constructor__)) static void(M_JOIN(_v,OUTMAIN_BEFORE))(void){OUTMAIN_BEFORE();}
     #endif 
     #ifdef OUTMAIN_AFTER
-        __attribute__((__destructor__)) static void M_JOIN(_OM_,M_PEAK(OUTMAIN_AFTER))(void){ M_REMAINED(OUTMAIN_AFTER); }
+        __attribute__((__destructor__)) static void(M_JOIN(_v,OUTMAIN_AFTER))(void){OUTMAIN_AFTER();}
     #endif 
 #endif
 
-#if _OUTMAIN_SYNTAX == 2 /* VS */
-
-#endif
-
-#if _OUTMAIN_SYNTAX == 3 /*  */
+/*****************   VC   **************/
+#if _OUTMAIN_SYNTAX == 2 
     #ifdef OUTMAIN_BEFORE
-        static void M_JOIN(_OM_,M_PEAK(OUTMAIN_BEFORE))(void){ M_REMAINED(OUTMAIN_BEFORE); }
-        #pragma startup M_JOIN(_OM_,M_PEAK(OUTMAIN_BEFORE))
-    #endif 
-    #ifdef OUTMAIN_AFTER
-        static void M_JOIN(_OM_,M_PEAK(OUTMAIN_AFTER))(void){ M_REMAINED(OUTMAIN_AFTER); }
-        #pragma end M_JOIN(_OM_,M_PEAK(OUTMAIN_AFTER))
-    #endif 
-#endif
-
-#if _OUTMAIN_SYNTAX == 4 /*  */
-    #ifdef OUTMAIN_BEFORE
-        void M_JOIN(_OM_,M_PEAK(OUTMAIN_BEFORE))(void){
-            __asm__ (".section .init \n call " M_STRING(M_JOIN(_OM_,M_PEAK(OUTMAIN_BEFORE))) " \n .section .text\n");
-            M_REMAINED(OUTMAIN_BEFORE);
-        }
+        #ifdef _WIN64
+            #pragma section(".CRT$XCU",read)
+            __declspec(allocate(".CRT$XCU"))
+        #else
+            #pragma data_seg(".CRT$XCU")
+        #endif
+            static void(*M_JOIN(_v,OUTMAIN_BEFORE))(void) = OUTMAIN_BEFORE;
+        #pragma data_seg()
     #endif
     #ifdef OUTMAIN_AFTER
-        void M_JOIN(_OM_,M_PEAK(OUTMAIN_AFTER))(void){
-            __asm__ (".section .fini \n call " M_STRING(M_JOIN(_OM_,M_PEAK(OUTMAIN_AFTER))) " \n .section .text\n");
-            M_REMAINED(OUTMAIN_AFTER);
-        }
+        #ifdef _WIN64
+            #pragma section(".CRT$XPU",read)
+            __declspec(allocate(".CRT$XPU"))
+        #else
+            #pragma data_seg(".CRT$XPU")
+        #endif
+            static void(*M_JOIN(_v,OUTMAIN_AFTER))(void) = OUTMAIN_AFTER;
+        #pragma data_seg()
+    #endif
+#endif
+
+/*****************   HZ   **************/
+#if _OUTMAIN_SYNTAX == 3
+    #ifdef OUTMAIN_BEFORE
+        #pragma startup OUTMAIN_BEFORE
+    #endif 
+    #ifdef OUTMAIN_AFTER
+        #pragma end OUTMAIN_AFTER
     #endif 
 #endif
 
-
+/*****************   ASM   **************/
+#if _OUTMAIN_SYNTAX == 4
+    #ifdef OUTMAIN_BEFORE
+        __asm__ (".section .init \n call " M_STRING(OUTMAIN_BEFORE) " \n .section .text\n");
+    #endif
+    #ifdef OUTMAIN_AFTER
+        __asm__ (".section .fini \n call " M_STRING(OUTMAIN_AFTER) " \n .section .text\n");
+    #endif 
+#endif
 
 
 #ifdef OUTMAIN_BEFORE
@@ -61,4 +76,3 @@
 #ifdef OUTMAIN_AFTER
     #undef OUTMAIN_AFTER
 #endif
-
