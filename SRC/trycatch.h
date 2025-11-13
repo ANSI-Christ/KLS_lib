672992c119faa669f860c19a7993a794863c17d4
Klaus <ansichrist@yandex.ru>
2025-11-13 22:35:34 +0300

    Update trycatch.h

--- SRC/trycatch.h
+++ SRC/trycatch.h
@@ -13,6 +13,8 @@
 
 #include "macro.h"
 
+/* if the compiler does not support the declaration in a loop, then compile with the flag -DTRY_CATCH_NO_LOOP_DECL */
+
 /*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
 /*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
 
@@ -21,7 +23,7 @@ extern const struct EXCEPTION_INFO{
     const char *where;
 } * const EXCEPTION;
 
-#define TRY(...)     for(;_TRY(__VA_ARGS__);THROW())if(!1);
+#define TRY(...)     _TRY(__VA_ARGS__)
 #define CATCH(...)   M_IF(M_IS_ARG(M_PEAK(__VA_ARGS__)))(_CATCH1,_CATCH0)(__VA_ARGS__)
 #define THROW(...)   M_IF(M_IS_ARG(M_PEAK(__VA_ARGS__)))(_THROW1,_THROW0)(__VA_ARGS__)
 #define DEBUG(...)   TRY(__VA_ARGS__)CATCH()(printf("\nDEBUG[%s:%d] %s at %s\n",M_FILE(),M_LINE(),EXCEPTION->type,EXCEPTION->where); getchar();)
@@ -34,12 +36,7 @@ extern void(*TryCatchTerminate)(void);  /* by default exit(-1) */
 
 struct _TRYCATCH{ jmp_buf *jmp; struct EXCEPTION_INFO info[1]; void *data; char buffer[96];};
 struct _TRYCATCH *_TryCatch(char);
-#define EXCEPTION ((const struct EXCEPTION_INFO*)((const struct _EXCEPTION_DONT_EXISTS*)_5tc_->info))
-#define _CATCH0() else{ struct _TRYCATCH * const _5tc_=_TryCatch(0); _CATCH_ELSE
-#define _CATCH1(_type_,...) else if( ({ struct _TRYCATCH * const _5tc_=_TryCatch(0); if(!strcmp(_5tc_->info->type,M_STRING(_type_))){ M_WHEN(M_IS_ARG(M_PEAK(__VA_ARGS__)))( _type_ __VA_ARGS__= *(_type_*)(_5tc_->data); ) _CATCH_ELIF
-#define _CATCH_DO(...) do{ struct _EXCEPTION_DONT_EXISTS{char _;}; __VA_ARGS__ }while(!1); break; if(_5tc_->jmp) continue;
-#define _CATCH_ELIF(...) _CATCH_DO(__VA_ARGS__) } 0;}) );
-#define _CATCH_ELSE(...) _CATCH_DO(__VA_ARGS__) }
+#define EXCEPTION ((const struct EXCEPTION_INFO*)((const struct _EXCEPTION_DONT_EXISTS*)_1tc_->info))
 #define _THROW_INFO M_FILE() ":" M_STRING(M_LINE())
 #define _THROW0() ({\
     struct _TRYCATCH * const _1_=_TryCatch(0);\
@@ -66,17 +63,35 @@ struct _TRYCATCH *_TryCatch(char);
     }puts("\nterminate called after throwing an instance of \'" M_STRING(_type_) "\' at [" _THROW_INFO "]\n");\
     TryCatchTerminate();\
 })
-#define _TRY(...) ({\
-    jmp_buf _1tc_, *_2tc_;\
-    struct _TRYCATCH * const _3tc_=_TryCatch(1);\
+#define __TRY(_decl_,...) ({\
+    jmp_buf _2tc_, *_3tc_;\
+    _decl_;\
     char _4tc_;\
-    if(!_3tc_){puts("\n\nTRY FAULT at [" _THROW_INFO "]\n");TryCatchTerminate();}\
-    _2tc_=_3tc_->jmp;\
-    _3tc_->jmp=&_1tc_;\
-    _4tc_=setjmp(_1tc_);\
+    if(!_1tc_){puts("\n\nTRY FAULT at [" _THROW_INFO "]\n");TryCatchTerminate();}\
+    _3tc_=_1tc_->jmp;\
+    _1tc_->jmp=&_2tc_;\
+    _4tc_=setjmp(_2tc_);\
     if(!_4tc_) do{__VA_ARGS__}while(!5);\
-    _3tc_->jmp=_2tc_; _4tc_;\
+    _1tc_->jmp=_3tc_; _4tc_;\
 })
+
+#ifdef TRY_CATCH_NO_LOOP_DECL
+
+#define _CATCH0() else{ struct _TRYCATCH * const _1tc_=_TryCatch(0); _CATCH_ELSE
+#define _CATCH1(_type_,...) else if( ({ struct _TRYCATCH * const _1tc_=_TryCatch(0); if(!strcmp(_1tc_->info->type,M_STRING(_type_))){ M_WHEN(M_IS_ARG(M_PEAK(__VA_ARGS__)))( _type_ __VA_ARGS__= *(_type_*)(_1tc_->data); ) _CATCH_ELIF
+#define _CATCH_DO(...) do{ struct _EXCEPTION_DONT_EXISTS{char _;}; __VA_ARGS__ }while(!1); break; if(_1tc_->jmp) continue;
+#define _CATCH_ELIF(...) _CATCH_DO(__VA_ARGS__) } 0;}) );
+#define _CATCH_ELSE(...) _CATCH_DO(__VA_ARGS__) }
+#define _TRY(...) for(;__TRY(struct _TRYCATCH * const _1tc_=_TryCatch(1),__VA_ARGS__);THROW())if(!1);
+
+#else
+
+#define _CATCH(...) do{ struct _EXCEPTION_DONT_EXISTS{char _;}; __VA_ARGS__ }while(!1); break; }
+#define _CATCH0() else{ _CATCH
+#define _CATCH1(_type_,...) else if(!strcmp(_1tc_->info->type,M_STRING(_type_))){ M_WHEN(M_IS_ARG(M_PEAK(__VA_ARGS__)))( _type_ __VA_ARGS__= *(_type_*)(_1tc_->data); ) _CATCH
+#define _TRY(...) for(struct _TRYCATCH * const _1tc_=_TryCatch(1);__TRY(,__VA_ARGS__);THROW())if(!1);
+
+#endif
 
 #endif /* TRY_CATCH_H */
 
