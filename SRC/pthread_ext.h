19205ea47c893c3f6fa020440f4bc6280ada0724
Klaus <ansichrist@yandex.ru>
2026-04-13 13:39:04 +0300

    Update pthread_ext.h

--- SRC/pthread_ext.h
+++ SRC/pthread_ext.h
@@ -427,8 +427,8 @@ typedef struct{
     pthread_pool_t * const pool;
     pthread_attr_t * const attr;
     pthread_t * const tid;
-    const unsigned int attr_inc, tid_cnt;
-    unsigned int wait;
+    const unsigned int inc;
+    unsigned int count;
 }_pthread_pool_initializer_t;
 
 typedef struct{char _; pthread_t t;}_pthread_pool_align_t;
@@ -488,11 +488,11 @@ static void _pthread_pool_release(pthread_pool_t * const p){
 static void *_pthread_pool_worker(_pthread_pool_initializer_t * const cfg){
     pthread_pool_t * const p=cfg->pool;
     const unsigned int index=p->count++;
-    unsigned int busy=(p->count==cfg->tid_cnt || pthread_create(cfg->tid+p->count,cfg->attr+p->count*cfg->attr_inc,(void*(*)(void*))_pthread_pool_worker,cfg));
+    unsigned int busy=(p->count==cfg->count || pthread_create(cfg->tid+p->count,cfg->attr+p->count*cfg->inc,(void*(*)(void*))_pthread_pool_worker,cfg));
     void(* const del)(void*)=p->deallocator;
 
     pthread_mutex_lock(p->mtx);
-    if(busy){cfg->wait=0; pthread_cond_signal(p->cond+1);}
+    if(busy){cfg->count=0; pthread_cond_signal(p->cond+1);}
     for(busy=0;;){
         _pthread_pool_task_t *t=_pthread_pool_pop(p);
         if(t){
@@ -589,17 +589,14 @@ pthread_pool_t *pthread_pool_create_ex(unsigned int count,const unsigned char pr
             if(((size_t)_p) & 255){p->ctrl|=16; ((void**)p)[-1]=_p;}
             memset(p->queue,0,qsize);
 
-            {_pthread_pool_initializer_t cfg[1]={{p,pattr,_pthread_pool_tids(p),pattrs>1,count,1}};
-            if(pthread_create(cfg->tid,cfg->attr,(void*(*)(void*))_pthread_pool_worker,cfg)){
-                pthread_pool_destroy(p,1); return NULL;
+            {_pthread_pool_initializer_t cfg[1]={{p,pattr,_pthread_pool_tids(p),pattrs>1,count}};
+            if(!pthread_create(cfg->tid,cfg->attr,(void*(*)(void*))_pthread_pool_worker,cfg)){
+                pthread_mutex_lock(p->mtx);
+                while(cfg->count) pthread_cond_wait(p->cond+1,p->mtx);
+                pthread_mutex_unlock(p->mtx);
+                if(p->count==count) return p;
             }
-            pthread_mutex_lock(p->mtx);
-            while(cfg->wait) pthread_cond_wait(p->cond+1,p->mtx);
-            pthread_mutex_unlock(p->mtx);
-            if(p->count!=count){
-                pthread_pool_destroy(p,1); return NULL;
-            }}
-            return p;
+            pthread_pool_destroy(p,1);}
         }
     } return NULL;
 }
