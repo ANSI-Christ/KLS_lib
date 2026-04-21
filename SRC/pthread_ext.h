da54a3e3ae4437b1aa4b6cb95c39b3e6ff87b70f
Klaus <ansichrist@yandex.ru>
2026-04-21 05:10:20 +0300

    Update pthread_ext.h

--- SRC/pthread_ext.h
+++ SRC/pthread_ext.h
@@ -638,8 +638,8 @@ int pthread_pool_task(pthread_pool_t * const p,void * const t,unsigned char prio
     pthread_mutex_lock(p->mtx);
     if( !(err=p->ctrl & 3) ){
         if(prio>p->peak) p->peak=prio;
-        ++p->size; _pthread_pool_append(p,(_pthread_pool_task_t*)t,prio);
-        if(p->busy!=p->count) pthread_cond_signal(p->cond);
+        _pthread_pool_append(p,(_pthread_pool_task_t*)t,prio);
+        if(p->size++<p->count-p->busy) pthread_cond_signal(p->cond);
     }
     pthread_mutex_unlock(p->mtx);
     if(err & 1) return EINVAL;
@@ -650,8 +650,8 @@ int pthread_pool_task(pthread_pool_t * const p,void * const t,unsigned char prio
 void pthread_pool_urgent(pthread_pool_t * const p,void * const t){
     ((_pthread_pool_task_t*)t)->next=NULL;
     pthread_mutex_lock(p->mtx);
-    ++p->size; _pthread_pool_prepend(p,(_pthread_pool_task_t*)t,(p->peak=p->max));
-    if(p->busy!=p->count) pthread_cond_signal(p->cond);
+    _pthread_pool_prepend(p,(_pthread_pool_task_t*)t,(p->peak=p->max));
+    if(p->size++<p->count-p->busy) pthread_cond_signal(p->cond);
     pthread_mutex_unlock(p->mtx);
 }
 
